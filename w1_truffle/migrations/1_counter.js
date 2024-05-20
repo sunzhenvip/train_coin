d89bb89216c00910c4842df2db51e6f08dd99154
test <test@qq.com>
2024-05-20 23:22:37 +0800

    调整

--- w1_truffle/migrations/1_counter.js
+++ w1_truffle/migrations/1_counter.js
@@ -1,5 +1,7 @@
 const Counter = artifacts.require("Counter");
 
 module.exports = function (deployer) {
-  deployer.deploy(Counter, 2);
+  deployer.deploy(Counter, 2).then((instance) => {
+    console.log("合约地址 Deployed contract address:", instance.address);
+  });
 };
