4f1676e395836d4462ec244386ee590fc93d37d6
test <test@qq.com>
2024-05-23 23:24:50 +0800

    脚本部署测试

---
+++ w1_hardhat/deploy_with_param.sh
@@ -0,0 +1,8 @@
+
+
+# 脚本方式部署
+# linux 或 mac
+export HARDHAT_NETWORK='localdev'
+# Windows 可以使用下面的这个命令
+# set HARDHAT_NETWORK=localdev
+node scripts/deploy_by_param.js 10
---
+++ w1_hardhat/scripts/deploy_by_param.js
@@ -0,0 +1,44 @@
+const hre = require("hardhat");
+const { ethers, network, artifacts } = require("hardhat");
+
+
+
+const { writeAbiAddr } = require('./artifact_saver.js');
+
+const prams = process.argv
+const value = prams[2]
+
+console.log("Counter deploy with value:", value);
+
+async function main() {
+  await hre.run('compile');
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  console.log("Lock deployed to:", lock.address);
+
+
+  let Artifact = await artifacts.readArtifact("Lock");
+  await writeAbiAddr(Artifact, lock.address, "Lock", network.name);
+
+  console.log(`Please verify: npx hardhat verify ${lock.address}`);
+
+
+  // let Artifact = await artifacts.readArtifact("Lock");
+  // await writeAbiAddr(Artifact, counter.address, "Lock", network.name);
+
+  // console.log(`Please verify: npx hardhat verify ${counter.address}`);
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
