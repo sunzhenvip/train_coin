77a1b75317687f17368554866d4285c521847f3a
test <test@qq.com>
2025-01-01 22:46:52 +0800

    desc

--- C2N-Launchpad/c2n-contracts/contracts/Airdrop.sol
+++ C2N-Launchpad/c2n-contracts/contracts/Airdrop.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 
-
+// 空投
 contract Airdrop {
 
     using ECDSA for bytes32;
