1931338ae6a626750896aa6cc8737702607b5a3e
test <test@qq.com>
2025-03-20 22:48:58 +0800

    farm质押部署以及注意事项

--- renke/C2N-launchpad/c2n-contracts/scripts/deployment/deploy_farm.js
+++ renke/C2N-launchpad/c2n-contracts/scripts/deployment/deploy_farm.js
@@ -4,7 +4,7 @@ const {ethers} = require("hardhat");
 
 async function main() {
     const RPS = "1";
-    const startTS= 1731761560;
+    const startTS= 1742482160; // 当前时间增加个100秒左右
     // get c2n token address from contract address file
     const c2nTokenAddress = getSavedContractAddresses()[hre.network.name]["C2N-TOKEN"];
     console.log("c2nTokenAddress: ", c2nTokenAddress)
